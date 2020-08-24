b99c1decb8e2707eab357d3a5160ebf0bc2bf872
rofe-dl <rafidul258@gmail.com>
2020-08-24 19:12:09 +0600

    downloads more than 51 maps

--- OSUDownloader.py
+++ OSUDownloader.py
@@ -13,27 +13,36 @@ user_id = int(input('Enter User ID from profile URL: '))
 number_of_maps = int(input('Enter top number of maps to download: '))
 osu_session_cookie = str(input('Enter osu session token, instructions in github readme: '))
 
-r = requests.get(f'https://osu.ppy.sh/users/{user_id}/beatmapsets/most_played?offset=0&limit={number_of_maps}')
-data = r.json()
+offset = 0
 
-try:
-    os.makedirs("./songs")
-except FileExistsError:
-    pass
+while(number_of_maps > 0):
 
-for beatmap in data:
-    beatmap_id = beatmap['beatmapset']['id']
-    beatmap_title = removeDisallowedFilenameChars(str(beatmap['beatmapset']['title']))
-    download_url = f"https://osu.ppy.sh/beatmapsets/{beatmap_id}/download?noVideo=1"
+    r = requests.get(f'https://osu.ppy.sh/users/{user_id}/beatmapsets/most_played?offset={offset}&limit={number_of_maps}')
+    data = r.json()
+
+    try:
+        os.makedirs("./songs")
+    except FileExistsError:
+        pass
+
+    for beatmap in data:
+        beatmap_id = beatmap['beatmapset']['id']
+        beatmap_title = removeDisallowedFilenameChars(str(beatmap['beatmapset']['title']))
+        download_url = f"https://osu.ppy.sh/beatmapsets/{beatmap_id}/download?noVideo=1"
+        
+        print(f'\n-------{beatmap_id}-------')
+        print(download_url)
+        print('Downloading beatmap: ' + str(beatmap_title))
+        
+        cookies = {'osu_session': osu_session_cookie}
+        r = requests.get(download_url, cookies=cookies)
+
+        with open(f'./songs/{beatmap_title}.osz', 'wb') as f:  
+            f.write(r.content)
+        
+    number_of_maps -= 51 # number of maps left
+    offset += 51 # site only allows 51 downloads each request
+
     
-    print(f'\n-------{beatmap_id}-------')
-    print(download_url)
-    print('Downloading beatmap: ' + str(beatmap_title))
-    
-    cookies = {'osu_session': osu_session_cookie}
-    r = requests.get(download_url, cookies=cookies)
-
-    with open(f'./songs/{beatmap_title}.osz', 'wb') as f:  
-        f.write(r.content)
 
         
