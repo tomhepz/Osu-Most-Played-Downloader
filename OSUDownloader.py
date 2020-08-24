3bd1bc59d09c3b0541b8e51f98915bbae5b61305
rofe-dl <rafidul258@gmail.com>
2020-08-24 19:25:38 +0600

    7s delay each download, kept count of downloads

--- OSUDownloader.py
+++ OSUDownloader.py
@@ -3,6 +3,7 @@ import requests
 import os
 import string
 import unicodedata
+import time
 
 validFilenameChars = "-_.() %s%s" % (string.ascii_letters, string.digits)
 def removeDisallowedFilenameChars(filename):
@@ -12,6 +13,10 @@ def removeDisallowedFilenameChars(filename):
 user_id = int(input('Enter User ID from profile URL: '))
 number_of_maps = int(input('Enter top number of maps to download: '))
 osu_session_cookie = str(input('Enter osu session token, instructions in github readme: '))
+
+print('\nThere will be a 7 second delay between each download to prevent error for too many requests')
+
+beatmap_count = 0
 
 offset = 0
 
@@ -26,13 +31,19 @@ while(number_of_maps > 0):
         pass
 
     for beatmap in data:
+
+        beatmap_count += 1
+
         beatmap_id = beatmap['beatmapset']['id']
         beatmap_title = removeDisallowedFilenameChars(str(beatmap['beatmapset']['title']))
+
+        if beatmap_count != 1 : time.sleep(7) # delay put to prevent error 429, delay skipped for the first download
+
         download_url = f"https://osu.ppy.sh/beatmapsets/{beatmap_id}/download?noVideo=1"
         
         print(f'\n-------{beatmap_id}-------')
         print(download_url)
-        print('Downloading beatmap: ' + str(beatmap_title))
+        print(f'Downloading beatmap: {beatmap_count}. ' + str(beatmap_title))
         
         cookies = {'osu_session': osu_session_cookie}
         r = requests.get(download_url, cookies=cookies)
