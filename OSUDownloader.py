d8452629c134f73b92718339f4167ba33bfb7361
Jon <jon.cy.lee98@gmail.com>
2022-12-31 10:26:36 -0500

    Adds multiple new features and bug fixes - Allows users to download more than the top 100 most played - Fixes bug where requests get blocked due to api limit - Will download beatmaps that are no longer accessible via mirror link - Displays any beatmaps that are not able to be downloaded due to issues

--- OSUDownloader.py
+++ OSUDownloader.py
@@ -1,8 +1,8 @@
-import json
 import requests
 import os
 import string
 import unicodedata
+import time
 
 validFilenameChars = "-_.() %s%s" % (string.ascii_letters, string.digits)
 def removeDisallowedFilenameChars(filename):
@@ -13,27 +13,60 @@ user_id = int(input('Enter User ID from profile URL: '))
 number_of_maps = int(input('Enter top number of maps to download: '))
 osu_session_cookie = str(input('Enter osu session token, instructions in github readme: '))
 
-r = requests.get(f'https://osu.ppy.sh/users/{user_id}/beatmapsets/most_played?offset=0&limit={number_of_maps}')
-data = r.json()
+cookies = {'osu_session': osu_session_cookie}
+dirname = os.path.realpath(os.path.dirname(__file__))
 
-try:
-    os.makedirs("./songs")
-except FileExistsError:
-    pass
+os.makedirs(f"{dirname}/songs", exist_ok = True)
+print(f"Download directory is stored in: {dirname}/songs")
 
-for beatmap in data:
-    beatmap_id = beatmap['beatmapset']['id']
-    beatmap_title = removeDisallowedFilenameChars(str(beatmap['beatmapset']['title']))
-    download_url = f"https://osu.ppy.sh/beatmapsets/{beatmap_id}/download?noVideo=1"
-    
-    print(f'\n-------{beatmap_id}-------')
-    print(download_url)
-    print('Downloading beatmap: ' + str(beatmap_title))
-    
-    cookies = {'osu_session': osu_session_cookie}
-    r = requests.get(download_url, cookies=cookies)
+unable_to_download = {}
+offset = 0
 
-    with open(f'./songs/{beatmap_title}.osz', 'wb') as f:  
-        f.write(r.content)
+# osu api only allows 100 maps per request
+# so we have to make multiple requests if the user has more than 100 maps
+# the offset is the number of maps to skip
+while number_of_maps > 0:
+    limit = 100 if number_of_maps >= 100 else number_of_maps
+    requesturl = f'https://osu.ppy.sh/users/{user_id}/beatmapsets/most_played?offset={offset}&limit={limit}'
 
+    r = requests.get(requesturl)
+    data = r.json()
+
+    for beatmap in data:
+        beatmap_id = beatmap['beatmapset']['id']
+        beatmap_title = removeDisallowedFilenameChars(str(beatmap['beatmapset']['title']))
         
+        beatmap_url = f"https://osu.ppy.sh/beatmapsets/{beatmap_id}"
+        download_url = f"{beatmap_url}/download?noVideo=1"
+
+        # Download only if the beatmap isn't already downloaded
+        if not os.path.isfile(f'{dirname}/songs/{beatmap_title}.osz'):
+            try:
+                r = requests.get(beatmap_url, cookies=cookies, headers = {'User-agent': 'osudownloader'})
+                # Checks to see if the beatmap is downloadable, if not then use beatconnect as a fallback
+                if "\"download_disabled\":true" in r.text:
+                    download_url = f"https://beatconnect.io/b/{beatmap_id}"
+            except Exception as e:
+                unable_to_download[beatmap_title] = beatmap_url
+                time.sleep(3)
+                continue
+
+            print(f'\n-------{beatmap_id}-------')
+            print(download_url)
+            print('Downloading beatmap: ' + str(beatmap_title))
+            try:
+                r = requests.get(download_url, cookies=cookies, headers = {'User-agent': 'osudownloader'})
+                with open(f'{dirname}/songs/{beatmap_title}.osz', 'wb') as f:  
+                    f.write(r.content)
+            except Exception as e:
+                unable_to_download[beatmap_title] = beatmap_url
+            # Sleep for 3 seconds to prevent getting rate limited
+            time.sleep(3)
+    number_of_maps -= 100
+    offset += 100
+
+# Print out the beatmaps that couldn't be downloaded
+if unable_to_download:
+    print("Unable to download these songs:")
+    for beatmap_title, beatmap_url in unable_to_download.items():
+        print(beatmap_title + ":" + beatmap_url)
